d922430cdebb90c6e4c8a943ace3e3e4be97a979
kingmarto12 <obedekem@gmail.com>
2023-09-08 02:39:10 -0700

    print f

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print new
+ * Return: 0 run sucefull
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
+
+
