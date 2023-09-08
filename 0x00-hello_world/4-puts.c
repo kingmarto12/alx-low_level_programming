175434e4ee26a373928e383e4d898d7c48cef3f0
kingmarto12 <obedekem@gmail.com>
2023-09-08 01:46:41 -0700

    c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main funtion
+ * Return: always 0
+ */
+
+int main(void)
+{
+        puts("\"Programming is like build
+        return (0);
+}       
