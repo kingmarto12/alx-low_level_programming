0d51bdc60df370bf99a887640115bf95fbc4ba9b
kingmarto12 <obedekem@gmail.com>
2023-09-09 14:03:15 -0700

    qote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
  * main - Entry point
  * Return: Always 1 (Success)
  */
+
 int main(void)
 {
 	write(STDOUT_FILENO, "and that piece of art is useful
