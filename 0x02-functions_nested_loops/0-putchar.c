0578d029a1e989ed57246119a1cac810e136488a
kingmarto12 <obedekem@gmail.com>
2023-09-12 20:32:36 -0700

    write

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - check
+ * description - It print the word _putchar, and return to a new line
+ * Return: 0;
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
